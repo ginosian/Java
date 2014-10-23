b4602f4eef77c421685260cfdda29d61daf7a418
ginosian <marta.ginosian@gmail.com>
2014-10-23 09:40:13 +0400

    23.10.2014

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+/**
+ * Created by Martha on 22.10.14.
+ */
+public class Main {
+    public static void main(String args []){
+        /** Math game
+         * demonstrates integer math using arithmetic operations
+         */
+        /*int num1, num2;
+        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+        try {
+            System.out.println("First number");
+            num1 = Integer.parseInt(reader.readLine());
+            System.out.println("Second number");
+            num2 = Integer.parseInt(reader.readLine());
+
+       }
+       catch (IOException ioe){
+        System.out.println("I/O error occurred, using 1    ");
+        num1 = num2 = 1;
+       }
+        catch (NumberFormatException nfe){
+            System.out.println("Number format incorrect using 1    ");
+            num1 = num2 =1;
+
+        }
+        System.out.println(num1 + "+ " + num2 + "= " + (num1+num2));
+        System.out.println(num1 + "- " + num2 + "= " + (num1-num2));
+        System.out.println(num1 + "/ " + num2 + "= " + (num1/num2));
+        System.out.println(num1 + "* " + num2 + "= " + (num1*num2));
+        */
+        /** Tip calculator
+         * Parses user input and floating point math
+         */
+
+        /*String costResponse;
+        Double tip, meal, total;
+        BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
+        System.out.println("How mauch was the meal");
+        try{
+            costResponse = reader.readLine();
+            meal = Double.parseDouble(costResponse);
+            tip = meal*0.15;
+            total = meal + tip;
+            System.out.println("The meal cost $" + meal);
+            System.out.println("The tip cost $" + tip);
+            System.out.println("The total is $" + total);
+        }
+        catch (IOException ioe){
+            System.out.println("I/O Exeption");
+        }
+        */
+
+        /**
+         * Hello Arg
+         * uses command line argument
+         */
+        /*
+        for (int i = 0; i<args.length; i++)
+        {
+            System.out.println("Hell " + args[i] + "!" );
+        }
+        */
+
+
+
+
+
+    }
+}
